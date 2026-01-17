1686db6d0f9a88522ed539fe40501a8ae5be3831
golden-gecko <golden-gecko@holisz.pl>
2026-01-17 23:34:23 +0100

    Add objects to map

--- projects/tests/src/Main.cpp
+++ projects/tests/src/Main.cpp
@@ -3,7 +3,7 @@
 int main(int argc, char* argv[])
 {
     ::testing::InitGoogleTest(&argc, argv);
-    ::testing::GTEST_FLAG(filter) = "resources.*";
+    ::testing::GTEST_FLAG(filter) = "*.*";
 
     int result = RUN_ALL_TESTS();
 
